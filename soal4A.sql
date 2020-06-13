d8e770c018ac9dd9ab0f90ed28f107bf0853d8f9
@ali25rohmansyah <ali25rohmansyah@gmail.com>
2020-06-14 02:26:22 +0700

    membersihkan code

--- soal4A.sql
+++ soal4A.sql
@@ -1,8 +1,8 @@
 -- membuat table user
-CREATE TABLE users( id INTEGER PRIMARY KEY, name VARCHAR(20), email VARCHAR(225) NOT NULL);
+CREATE TABLE users( id INTEGER AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), email VARCHAR(225) NOT NULL);
 
 -- membuat table news
-CREATE TABLE news ( id INTEGER PRIMARY KEY , title VARCHAR(20) NOT NULL , image VARCHAR(255) NOT NULL , deskripsi VARCHAR(50) NOT NULL , creat_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , User_id INTEGER, FOREIGN KEY (User_id) REFERENCES users(id));
+CREATE TABLE news ( id INTEGER AUTO_INCREMENT PRIMARY KEY , title VARCHAR(20) NOT NULL , image VARCHAR(255) NOT NULL , deskripsi VARCHAR(50) NOT NULL , creat_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , User_id INTEGER, FOREIGN KEY (User_id) REFERENCES users(id));
 
 -- menampilkan semua berita dengan user/penulis
 select * FROM news INNER JOIN users ON news.User_id = users.id;
@@ -12,4 +12,6 @@ SELECT * FROM news WHERE User_id = 1;
 
 -- menampilkan detail berita
 SELECT * FROM news WHERE id = 1;
+
+
 
